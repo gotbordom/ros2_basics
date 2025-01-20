b278955b9fd4b4d2c2665a0f5250dbdf1008f156
Anthony <gotbordom@gmail.com>
2025-01-20 22:46:42 +0000

    added a debugging publisher that I will use later for wall being followed.

--- src/wall_following_pkg/src/wall_follower.cpp
+++ src/wall_following_pkg/src/wall_follower.cpp
@@ -15,17 +15,14 @@
 
 // project headers
 
-/// @brief Enum to make code clearer when determining direction of wall following in later code.
-enum class WallFollowingDirection
-{
-  LeftHandSide = 0,
-  RightHandSide = 1
-};
+/// @brief Enum to make code clearer when determining direction of wall
+/// following in later code.
+enum class WallFollowingDirection { LeftHandSide = 0, RightHandSide = 1 };
 
 /// @brief WallFollowerNode class that handles listening to the laser scanner
-///        then determining next velocity command, and publishing it to the correct topic.
-class WallFollowerNode : public rclcpp::Node
-{
+///        then determining next velocity command, and publishing it to the
+///        correct topic.
+class WallFollowerNode : public rclcpp::Node {
 public:
   WallFollowerNode(WallFollowingDirection wall_to_follow =
                        WallFollowingDirection::LeftHandSide,
@@ -34,59 +31,74 @@ public:
                    std::string publisher_channel = "cmd_vel")
       : Node(node_name), wall_to_follow_(wall_to_follow),
         subscription_channel_(subscription_channel),
-        publisher_channel_(publisher_channel)
-  {
+        publisher_channel_(publisher_channel) {
 
     // Create pub / sub
     subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         subscription_channel_, 10,
         std::bind(&WallFollowerNode::topic_callback, this,
                   std::placeholders::_1));
+
     publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
         publisher_channel_, 10);
 
-    debug_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>("debug_scan", 10);
+    wall_publisher_ =
+        this->create_publisher<sensor_msgs::msg::LaserScan>("debug_scan", 10);
   }
 
 private:
-  /// @brief Topic callback function that will be called everytime this node sees a new message on the @subscriber_channel_
-  ///        it will also publish data to the @publisher_channel_ indicating new drive velocities.
+  /// @brief Topic callback function that will be called everytime this node
+  /// sees a new message on the @subscriber_channel_
+  ///        it will also publish data to the @publisher_channel_ indicating new
+  ///        drive velocities.
   /// @param msg the last published LaserScan object
   auto topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
-      -> void
-  {
-    RCLCPP_INFO(this->get_logger(), "Got range of size: %d\n",
-                msg->ranges.size());
+      -> void {
 
-    auto ranges = msg->ranges;
-    auto total_ranges = msg->ranges.size();
+    // NOTE: Unfortunately I can't find documentation on this laser scanner and
+    // if angle 0 is ranges[0]... angle 360 is ranges[360]
 
-    // ------------- DEBUGGING LASER SCAN MSG ------------------
-    // NOTE: Unfortunately I can't find documentation on this laser scanner and if angle 0 is ranges[0]... angle 360 is ranges[360]
-    // ASSUMPTION: For now I will assume ranges[0] is distance at theta 0 where directly forward and it all increments CCW
-    // TODO (AT) DEBUGGING: I want to know where origin is.
-    auto debug_scan_msg = msg.get();
-    debug_scan_msg.ranges = debug_scan_msg.ranges[0];
-    debug_publisher_.publish(debug_scan_msg);
+    // ASSUMPTION:
+    // 0 degrees is forward
+    // 0 degress DIST is stored in msg->ranges[0]
+    // Degrees increment CCW such that msg->ranges[180] would be backward
+
+    // ANSWER:
+    // Looking at the data with a debugging topic and rviz2, this assumption is
+    // true.
+
+    auto filtered_scan = *msg;
+    for (size_t i = 0; i < filtered_scan.ranges.size(); ++i) {
+      float angle = msg->angle_min + i * msg->angle_increment;
+      // If angle is outside 0->pi set it to inf
+      if (angle > 3.14) {
+        filtered_scan.ranges[i] = std::numeric_limits<float>::infinity();
+      }
+    }
+    wall_publisher_->publish(filtered_scan);
+
     // ------------- DEBUGGING LASER SCAN MSG ------------------
 
     // split ranges into left
 
     // clean up range data
-    auto it = std::remove_if(ranges.begin(), ranges.end(), [&msg](float num)
-                             { return (num >= msg.range_max || num <= msg.range_min); });
-    ranges.erase(it, ranges.end());
+    // auto it = std::remove_if(ranges.begin(), ranges.end(), [&msg](float num)
+    // {
+    //   return (num >= msg.range_max || num <= msg.range_min);
+    // });
+    // ranges.erase(it, ranges.end());
   }
 
   WallFollowingDirection wall_to_follow_;
   std::string subscription_channel_, publisher_channel_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr debug_publisher_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+
+  // DEBUGGING OUTPUT
+  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr wall_publisher_;
 };
 
-auto main(int argc, char *argv[]) -> int
-{
+auto main(int argc, char *argv[]) -> int {
   // init the rcl
   rclcpp::init(argc, argv);
   // spin the node
