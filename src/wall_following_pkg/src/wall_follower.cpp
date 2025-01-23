5b6a815fb309c5f52bc53339a64fea37e7bc98ca
Anthony <gotbordom@gmail.com>
2025-01-23 21:26:11 +0000

    code compiles again and laser scan setup is working

--- src/wall_following_pkg/src/wall_follower.cpp
+++ src/wall_following_pkg/src/wall_follower.cpp
@@ -26,14 +26,9 @@ using namespace std::chrono_literals;
 
 /// @brief Enum to make code clearer when determining direction of wall
 ///        following in later code.
-enum class WallFollowingDirection
-{
-  LeftHandSide = 0,
-  RightHandSide = 1
-};
+enum class WallFollowingDirection { LeftHandSide = 0, RightHandSide = 1 };
 
-struct RangeInfos
-{
+struct RangeInfos {
   // Data that should only ever be updated in setup
   // range pairs are [ (inclusive), (exclusive) ]
   std::vector<std::pair<int, int>> front_idxs;
@@ -54,14 +49,13 @@ struct RangeInfos
   float min_front_range;
   float min_left_range;
   float min_right_range;
-}
+};
 
-struct RobotState
-{
+struct RobotState {
   RangeInfos range_infos;
   geometry_msgs::msg::Pose pose;
   WallFollowingDirection direction_to_follow;
-  WallFollowingRanges ranges_info;
+  RangeInfos ranges_info;
   float euclidean_dist_front;
   float euclidean_dist_left;
   float euclidean_dist_right;
@@ -72,12 +66,11 @@ struct RobotState
 ///        correct topic.
 ///        The aim is the have the robot follow the wall at a distance of
 ///        approximately 0.2 -> 0.3 meters
-class WallFollowerNode : public rclcpp::Node
-{
+class WallFollowerNode : public rclcpp::Node {
 public:
   WallFollowerNode(std::string publisher_channel = "cmd_vel")
-      : Node("wall_follower_node"), laser_scan_setup_done_(false), odom_setup_done_(false)
-  {
+      : Node("wall_follower_node"), laser_scan_setup_done_(false),
+        odom_setup_done_(false) {
 
     callback_group_1_ = this->create_callback_group(
         rclcpp::CallbackGroupType::MutuallyExclusive);
@@ -113,11 +106,11 @@ public:
 private:
   /// @brief   The controller callback in charge of publishing velocity values
   ///          based on the current state of the robot.
-  auto controller_callback() -> void {};
+  auto controller_callback() -> void {}
 
   /// @brief   The odometry callback is in charge of determining the previous,
   ///          and current, state of the robot given sensor data
-  auto odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) -> void {};
+  auto odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) -> void {}
 
   /// @brief The laser scan callback is tasked with updating the state object
   ///        with the correct Left, Right and Front ranges as well as minimum
@@ -133,17 +126,17 @@ private:
   ///    Looking at the data with a debugging topic and rviz2, assumptions 1-3
   ///    are true.
   auto laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
-      -> void
-  {
-    if (!laser_scan_setup_done_)
-      laser_scan_setup_done_ = laser_scan_setup(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+      -> void {
+    if (!laser_scan_setup_done_) {
+      laser_scan_setup_done_ = laser_scan_setup(msg);
+    }
   }
 
   /// @brief   Run the setup for the laser scan callback. This will calculate
   ///          the correct index values, etc for each range and store them.
   /// @return  boolean - if configuration was successful.
-  auto laser_scan_setup(const sensor_msgs::msg::LaserScan::SharedPtr msg) -> bool
-  {
+  auto laser_scan_setup(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+      -> bool {
 
     curr_state_.range_infos.angle_rad_increment = msg->angle_increment;
     curr_state_.range_infos.angle_rad_min_max.first = msg->angle_min;
@@ -151,28 +144,25 @@ private:
     curr_state_.range_infos.range_meter_min_max.first = msg->range_min;
     curr_state_.range_infos.range_meter_min_max.second = msg->range_max;
 
-    curr_state_.range_infos.front_idxs = {
-        {0, 44},
-        {315, 359}};
+    curr_state_.range_infos.front_idxs = {{0, 44}, {315, 359}};
 
-    curr_state_.range_infos.left_idxs = {
-        {45, 134}};
+    curr_state_.range_infos.left_idxs = {{45, 134}};
 
-    curr_state_.range_infos.right_idxs = {
-        {225, 314}};
+    curr_state_.range_infos.right_idxs = {{225, 314}};
 
     curr_state_.range_infos.right_range = std::vector<float>(
-        msg->range.begin() + curr_state_.range_infos.right_idxs[0].first,
-        msg->being() + curr_state_.range_infos.right_idxs[0].second);
+        msg->ranges.begin() + curr_state_.range_infos.right_idxs[0].first,
+        msg->ranges.begin() + curr_state_.range_infos.right_idxs[0].second);
 
     curr_state_.range_infos.left_range = std::vector<float>(
-        msg->range.begin() + curr_state_.range_infos.left_idxs[0].first,
-        msg->being() + curr_state_.range_infos.left_idxs[0].second);
+        msg->ranges.begin() + curr_state_.range_infos.left_idxs[0].first,
+        msg->ranges.begin() + curr_state_.range_infos.left_idxs[0].second);
 
     // Get front, left and right range vectors and save start/ end idx for each.
     // for (size_t i = 0; i < msg->ranges.size() l++ i)
     // {
-    //   // TODO: If I end up needing to filter out improper data this could be an
+    //   // TODO: If I end up needing to filter out improper data this could be
+    //   an
     //   //       optimal time.
     //   float angle = msg->angle_min + i * msg->angle_increment;
 
@@ -207,8 +197,10 @@ private:
 
   RobotState curr_state_, prev_state_;
 
-  // TODO - I want think on who should own these. I don't believe they would be a part of the robot state?
-  //        my thought being that they are a part of the robot's controller state?
+  // TODO - I want think on who should own these. I don't believe they would be
+  // a part of the robot state?
+  //        my thought being that they are a part of the robot's controller
+  //        state?
   bool laser_scan_setup_done_, odom_setup_done_;
 
   rclcpp::CallbackGroup::SharedPtr callback_group_1_, callback_group_2_,
@@ -219,8 +211,7 @@ private:
   rclcpp::TimerBase::SharedPtr controller_timer;
 };
 
-auto main(int argc, char *argv[]) -> int
-{
+auto main(int argc, char *argv[]) -> int {
   rclcpp::init(argc, argv);
 
   auto wall_follower_node = std::make_shared<WallFollowerNode>();
