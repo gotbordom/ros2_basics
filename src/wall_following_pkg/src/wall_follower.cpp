9fae0d060580852fc8a1f100c5a631a0db1c25b2
Anthony <gotbordom@gmail.com>
2025-01-23 17:50:26 +0000

    setup to now run with multi threaded executor

--- src/wall_following_pkg/src/wall_follower.cpp
+++ src/wall_following_pkg/src/wall_follower.cpp
@@ -1,4 +1,5 @@
 // std headers
+#include <chrono>
 #include <functional>
 #include <limits>
 #include <memory>
@@ -10,24 +11,23 @@
 #include "rclcpp/callback_group.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/subscription.hpp"
+#include "rclcpp/timer.hpp"
 #include "rclcpp/utilities.hpp"
 #include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
-#include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 
 // project headers
 
+using std::chrono_literals;
+
 /// @brief Enum to make code clearer when determining direction of wall
 ///        following in later code.
-enum class WallFollowingDirection
-{
-  LeftHandSide = 0,
-  RightHandSide = 1
-};
-struct RobotState
-{
+enum class WallFollowingDirection { LeftHandSide = 0, RightHandSide = 1 };
+
+struct RobotState {
   geometry_msgs::msg::Pose pose;
   WallFollowingDirection direction_to_follow;
   std::vector<float> front_range;
@@ -46,63 +46,71 @@ struct RobotState
 ///        correct topic.
 ///        The aim is the have the robot follow the wall at a distance of
 ///        approximately 0.2 -> 0.3 meters
-class WallFollowerNode : public rclcpp::Node
-{
+class WallFollowerNode : public rclcpp::Node {
 public:
-  WallFollowerNode(WallFollowingDirection wall_to_follow =
-                       WallFollowingDirection::LeftHandSide,
-                   std::string node_name = "wall_follower_node",
-                   std::string subscription_channel = "scan",
-                   std::string publisher_channel = "cmd_vel")
-      : Node(node_name), wall_to_follow_(wall_to_follow),
-        subscription_channel_(subscription_channel),
-        publisher_channel_(publisher_channel),
-        estimated_orientation_degrees_(0.0)
-  {
+  WallFollowerNode(std::string publisher_channel = "cmd_vel")
+      : Node("wall_follower_node") {
 
-    // Create pub / sub
-    subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        subscription_channel_, 10,
-        std::bind(&WallFollowerNode::topic_callback, this,
-                  std::placeholders::_1));
+    callback_group_1_ = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+    callback_group_2_ = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+    callback_group_3_ = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
 
-    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
-        publisher_channel_, 10);
+    laser_scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "scan", 10,
+        std::bind(&WallFollowerNode::laser_scan_callback, this,
+                  std::placeholders::_1),
+        callback_group_1_);
 
-    // TODO - Lol... I need each of these in their own group so that they can
-    // run in parallel
-    side_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>(
-        "debug_side_scan", 10);
-    front_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>(
-        "debug_front_scan", 10);
+    odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 10,
+        std::bind(&WallFollowerNode::odom_callback, this,
+                  std::placeholders::_1),
+        callback_group_2_);
+
+    controller_timer = this->create_wall_timer(
+        1s, std::bind(&WallFollowerNode::controller_callback, this),
+        callback_group_3_);
+
+    cmd_vel_pub_ =
+        this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   }
 
 private:
   /// @brief   The controller callback in charge of publishing velocity values
   ///          based on the current state of the robot.
-  auto controller_callback() -> void {};
+  auto controller_callback() -> void{};
 
   /// @brief   The odometry callback is in charge of determining the previous,
   ///          and current, state of the robot given sensor data
-  auto odom_callback() -> void {};
+  auto odom_callback() -> void{};
 
-  /// @brief The laser scan callback is tasked with updating the state object with
-  ///        the correct Left, Right and Front ranges as well as minimum values for each.
-  auto laser_scan_callback() -> void {};
+  /// @brief The laser scan callback is tasked with updating the state object
+  /// with
+  ///        the correct Left, Right and Front ranges as well as minimum values
+  ///        for each.
+  auto laser_scan_callback() -> void{};
 
-  std::string subscription_channel_, publisher_channel_;
+  RobotState curr_state, prev_state_;
+
+  rclcpp::CallbackGroup::SharedPtr callback_group_1_, callback_group_2_,
+      callback_group_3_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::TimerBase::SharedPtr controller_timer;
 };
 
-auto main(int argc, char *argv[]) -> int
-{
-  // init the rcl
+auto main(int argc, char *argv[]) -> int {
   rclcpp::init(argc, argv);
-  // spin the node
-  rclcpp::spin(std::make_shared<WallFollowerNode>());
-  // shutdown the node
+
+  auto wall_follower_node = std::make_shared<WallFollowerNode>();
+  rclcpp::executors::MultiThreadedExecutor executor;
+
+  executor.add_node(wall_follower_node);
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
